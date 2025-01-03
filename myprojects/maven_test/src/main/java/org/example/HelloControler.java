448ed05d900228861a7b181cd8bc8e05e460950f
3mArY33 <martarymarczuk16@gmail.com>
2025-01-03 16:22:16 +0100

    Aktualizacja Maven

--- myprojects/maven_test/src/main/java/org/example/HelloControler.java
+++ myprojects/maven_test/src/main/java/org/example/HelloControler.java
@@ -1,4 +1,10 @@
 package org.example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 public class HelloControler {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
 }
